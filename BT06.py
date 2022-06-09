a280b7a71056d8aadfa024af7dcd3aff79cf7dfa
khanhpt74 <72086102+khanhpt74@users.noreply.github.com>
2022-06-09 21:46:18 +0700

    Add files via upload

---
+++ BT06.py
@@ -0,0 +1,26 @@
+class Node(object):
+    def __init__(self):
+        self.data = None # contains the data
+        self.next = None # contains the reference to the next node
+
+class LinkedList:
+    def __init__(self):
+        self.cur_node = None
+
+    def add_node(self, data):
+        new_node = Node() # create a new node
+        new_node.data = data
+        new_node.next = self.cur_node # link the new node to the 'previous' node.
+        self.cur_node = new_node #  set the current node to the new one.
+
+    def list_print(self):
+        node = self.cur_node # cant point to ll!
+        while node:
+            print (node.data)
+            node = node.next
+
+ll = LinkedList()
+ll.add_node(343)
+ll.add_node(546)
+ll.add_node(324)
+ll.list_print()
---
+++ BT08.py
@@ -0,0 +1,25 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def add(self, dataval):
+        if dataval not in self.stack:
+            self.stack.append(dataval)
+            return True
+        else:
+            return False
+    def peek(self):
+	    return self.stack[-1]
+
+AStack = Stack()
+AStack.add("Mon")
+AStack.add("Tue")
+AStack.peek()
+
+print(AStack.peek())
+print('-'*10)
+
+AStack.add("Wed")
+AStack.add("Thu")
+print(AStack.peek())
+
